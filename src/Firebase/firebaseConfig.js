2b0740438f64774d6265eaf21763dc3f1cf1dbfa
luisa urrego <luisafernandaurrego89@gmail.com>
2023-06-16 12:53:56 -0500

    creando estructura de carpetas

---
+++ src/Firebase/firebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpa2Cy7gc26ePs_PgjRsZqIlEhUdP3q5E",
+  authDomain: "delivery-food-f175e.firebaseapp.com",
+  projectId: "delivery-food-f175e",
+  storageBucket: "delivery-food-f175e.appspot.com",
+  messagingSenderId: "881381691947",
+  appId: "1:881381691947:web:ab8d125f026f31c0a5106e",
+  measurementId: "G-MKWV84W8X7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export const auth = getAuth (app)
